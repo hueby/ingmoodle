ccf315b0b1ede2c4c8d362202ab1e2215e22fc3a
Dennis Hübner <dennis.huebner89@gmail.com>
2017-03-31 14:29:20 +0200

    Implemented get_customers by mobile

--- www/addons/checklists/controllers/customers.js
+++ www/addons/checklists/controllers/customers.js
@@ -1,5 +1,10 @@
 angular.module('mm.addons.checklists')
 
-.controller('mmaChecklistsCustomersCtrl', function($scope, $stateParams) {
+.controller('mmaChecklistsCustomersCtrl', function($scope, $stateParams, $mmaChecklists) {
+    $scope.canSwipe = true;
+
+    $mmaChecklists.getCustomers().then(function(customers) {
+        $scope.customers = customers
+    });
     $scope.bums = "1 * (3 + 3) * 7 = 42"
 });
--- www/addons/checklists/services/checklists.js
+++ www/addons/checklists/services/checklists.js
@@ -5,8 +5,15 @@ angular.module('mm.addons.checklists')
 
 })
 
-.factory('$mmaChecklists', function($log) {
+.factory('$mmaChecklists', function($log, $mmSite) {
     $log = $log.getInstance('$mmaChecklists');
     var self = {}
+
+    self.getCustomers = function() {
+        $log.debug('Get customers');
+        
+        return $mmSite.read('local_checklists_get_customers');
+    }
+
     return self;
 });
