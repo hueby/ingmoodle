41932989cf1d4cbd372e7624d305b66ee157c02d
Dennis Hübner <dennis.huebner89@gmail.com>
2017-04-05 16:06:12 +0200

    customers insert works, but the list is not updating -.-

--- www/addons/checklists/controllers/customers.js
+++ www/addons/checklists/controllers/customers.js
@@ -1,7 +1,42 @@
 angular.module('mm.addons.checklists')
 
-.controller('mmaChecklistsCustomersCtrl', function($scope, $stateParams, $mmaChecklists) {
+.controller('mmaChecklistsCustomersCtrl', function($scope, $stateParams, $mmaChecklists, $ionicModal, $mmSite, $mmEvents, mmaChecklistsCustomersRefreshed, $ionicHistory) {
     $scope.canSwipe = true;
+
+    $scope.customers = [];
+
+    $ionicModal.fromTemplateUrl('templates/modal.html', {
+        scope: $scope
+    }).then(function(modal) {
+        $scope.modal = modal;
+    });
+
+    $scope.createCustomer = function(newuser) {
+        var newUser = {
+            firstname: newuser.firstName,
+            lastname: newuser.lastName,
+            email: newuser.email,
+            phonenumber: newuser.phone,
+            location: newuser.location,
+            username: 'test'
+        }
+        $mmSite.write('local_checklists_insert_customer', newUser).then(function(id) {
+            $scope.modal.hide();
+            newUser.id = id.id;
+            console.log(JSON.stringify($scope.customers));
+            $scope.customers.push(newUser);
+            console.log(JSON.stringify($scope.customers));
+        });
+    };
+
+    $scope.refreshCustomers = function() {
+        $scope.$broadcast('scroll.refreshComplete');
+        $mmaChecklists.getCustomers().then(function(customers) {
+            console.log(JSON.stringify($scope.customers));
+            $scope.customers = customers;
+            console.log(JSON.stringify($scope.customers));
+        });
+    };
 
     $mmaChecklists.getCustomers().then(function(customers) {
         $scope.customers = customers
