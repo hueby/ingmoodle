3bc9cedad4d0870fa21c416e3f8604e2189e0d0c
Dennis Hübner <dennis.huebner89@gmail.com>
2017-07-24 11:17:17 +0200

    Ionic gets me

--- www/addons/mobile/controllers/login.js
+++ www/addons/mobile/controllers/login.js
@@ -1,10 +1,20 @@
 angular.module('mm.addons.mobile')
 
-    .controller('mmaMobileLoginCtrl', function($scope, $ionicHistory, $state, $timeout, $stateParams, $mmaMobile, $ionicModal, $mmSite, $mmEvents) {
-        $scope.canSwipe = true;
-        $scope.doorimg = 'img/hodor.jpg';
+  .controller('mmaMobileLoginCtrl', function($scope, $ionicHistory, $state, $timeout, $stateParams, $mmaMobile, $ionicModal, $mmSite, $mmEvents) {
+    $scope.canSwipe = true;
+    $scope.doorimg = 'img/hodor.jpg';
 
-        $scope.login = function() {
-            $state.go("site.mm_courses");
+    $scope.login = function() {
+
+      $mmaMobile.getEnergyConsultant($mmSite.getUserId()).then(function(consultant) {
+        console.log("Consultant" + JSON.stringify(consultant));
+        console.log("MOODLE ID " + consultant[0].moodleid);
+
+        if(consultant[0].moodleid === "3") {
+          $state.go("site.consultant_dashboard");
         }
-    });
+      });
+
+      //$state.go("site.mm_courses");
+    }
+  });
--- www/core/lib/sitesfactory.js
+++ www/core/lib/sitesfactory.js
@@ -463,7 +463,7 @@ angular.module('mm.core')
             var site = this,
                 initialToken = site.token;
             data = data || {};
-            console.log(JSON.stringify(data));
+            console.log("DATAR: " + JSON.stringify(data));
             // Prevent calls with expired tokens.
             if (site.isTokenExpired()) {
                 $log.debug('Token expired, rejecting.');
@@ -490,7 +490,7 @@ angular.module('mm.core')
             preSets.wstoken = site.token;
             preSets.siteurl = site.siteurl;
             
-            console.log(JSON.stringify(preSets));
+            console.log("PRESETS: " + JSON.stringify(preSets));
 
             // Enable text filtering by default.
             data.moodlewssettingfilter = preSets.filter === false ? false : true;
